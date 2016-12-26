367fa23dba32805bebdfa702161f9cb87bd3b38b
Peder Voldnes Langdal <peder.langdal@gmail.com>
2016-12-26 18:45:10 +0100

    In the process of refactoring to a basic ECS

--- include/gfx.h
+++ include/gfx.h
@@ -6,6 +6,14 @@
 
 #define PX_PER_METER 50
 
+struct CircleRenderComponent {
+    float cx;   // Plan: Update this in physics system
+    float cy;
+    float angle;
+    sf::CircleShape shape;
+};
+
 int draw_world_polygons(sf::RenderWindow &ren, b2World *world);
+int RunCircleRenderSystem(sf::RenderWindow &ren, std::vector<CircleRenderComponent> comps);
 
 #endif /* ifndef L_GFX */
